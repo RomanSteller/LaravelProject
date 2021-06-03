8593a24027bc79d3b1d69fb5dd00e287ee68831c
Roman <romansteller.csgo@gmail.com>
2021-06-03 22:24:02 +0300

    created app.blade

--- resources/views/app/app.blade.php
+++ resources/views/app/app.blade.php
@@ -5,19 +5,17 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <title>Document</title>
 </head>
 <body>
 <header>
     <div class="container header_container">
         <div class="header_logo">
-            <a href=""><img src="assets/images/logo.png" alt=""></a>
+            <a href=""><img src="{{Storage::url('logo/logo.png')}}" alt=""></a>
         </div>
         <div class="header_user_nickname">
-            <div class="header_search">
-                <input type="text">
-            </div>
-            <h2>Steellsey</h2>
+            <p>Steellsey</p>
         </div>
     </div>
 </header>
