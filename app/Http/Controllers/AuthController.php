638ed77283adb444572b8e743b3785c54116f00e
Maxst <MaxSteellsey@gmail.com>
2021-06-04 03:19:30 +0300

    Normal`naya verstka

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AuthRequest;
+use App\Models\Articles;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function reg()
+    {
+        return view('auth.reg');
+    }
+
+    public function log()
+    {
+        return view('auth.auth');
+    }
+
+    public function createUser(Request $request){
+        $valid = $request->validate([
+            'login_input' => 'required|min:4|max:100',
+            'password_input' => 'required|min:6|max:50',
+            'mail_input' => 'required|email',
+            'user_agree_input' => 'required|accepted'
+        ]);
+
+        if ($request['password_repeat_input']=$request['password_input']){
+            $user = User::create([
+                'login' => $request['login_input'],
+                'password' => Hash::make($request['password_input']),
+                'passwordRepeat' => $request['password_repeat_input'],
+                'email' => $request['mail_input']
+
+                //'name' => 'AAA',//Надо изменить базу, а то ругается что отправляю null
+                //'role' => 'user',//Надо изменить базу, а то ругается что отправляю null
+                //'photo' => 'AAA'//Надо изменить базу, а то ругается что отправляю null
+            ]);
+
+            if($user){
+                return response()->json([
+                    $user,
+                    "message" => "Пользователь успешно зарегестрирован"
+                ])->setStatusCode(201);
+            }
+        }
+    }
+
+    public function authUser(Request $request){
+        $login = $request['login_input'];
+        $user = User::where('login',$login)->first();
+
+        if(!$user || !Hash::check($request['password_input'],$user['password'])){
+            return response()->json([
+                "message" => "Пользователь не найден"
+            ])->setStatusCode(401);
+        }else{
+            session(['id' => $user['id']]);
+            $id = session('id');
+            return response()->json([
+                $id
+            ]);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,9 +20,12 @@ use Illuminate\Support\Facades\Route;
 //});
 
 Route::get('/',[ArticleController::class,'allArticles'])->name('home');
-Route::get('/auth',function (){
-   return view('auth.auth');
-});
+
+Route::get('log',[AuthController::class,'log'])->name('authInput');
+Route::post('/auth/check', [AuthController::class,'authUser'])->name('auth');
+
+Route::get('reg',[AuthController::class,'reg'])->name('regInput');
+Route::post('/reg/check', [AuthController::class,'createUser'])->name('reg');
 
 //Route::get('/{any}',function (){
 //   return view('welcome');
