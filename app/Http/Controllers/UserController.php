9693a2eb69d8c672942f53974058ef90dfd45f18
Roman <romansteller.csgo@gmail.com>
2021-06-12 05:34:40 +0300

    ИСПРАВЛЕННЫЙ USERCONTROLLER

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,173 +11,91 @@ use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
-    public function index($id){
+    public function index($id)
+    {
         $user = User::find($id);
         $articlesChart = (new ArticleController)->articlesChart();
         $usersChart = (new ArticleController)->usersChart();
-        return view('profile.profile',compact('user','articlesChart','usersChart'));
+        return view('profile.profile', compact('user', 'articlesChart', 'usersChart'));
     }
 
-    public function getUserArticles($id,$statistic_name){
-        if($statistic_name === 'articles') {
+    public function getUserArticles($id, $statistic_name)
+    {
+        if ($statistic_name === 'articles') {
             $user = User::find($id);
             $articlesChart = (new ArticleController)->articlesChart();
             $articles = Articles::with('tags')->where('user_id', $id)->get();
             $usersChart = (new ArticleController)->usersChart();
-            return view('profile.userInfo',compact('articles','user','articlesChart','usersChart'));
+            return view('profile.userInfo', compact('articles', 'user', 'articlesChart', 'usersChart'));
         }
-        if($statistic_name === 'comments'){
+        if ($statistic_name === 'comments') {
             $user = User::find($id);
             $articlesChart = (new ArticleController)->articlesChart();
 
-            $comments = Comments::where('user_id',$id)->get();
-            foreach($comments as $comment){
+            $comments = Comments::where('user_id', $id)->get();
+            foreach ($comments as $comment) {
                 (new ArticleController)->dateOutput($comment);
             }
 
-            return view('profile.userInfo',compact('comments','user','articlesChart'));
+            return view('profile.userInfo', compact('comments', 'user', 'articlesChart'));
         }
     }
 
-    public function userSettingView(){
-        $user = User::where('id',$_SESSION['user']['id'])->first();
+    public function userSettingView()
+    {
+        $usersChart = (new ArticleController)->usersChart();
+        $user = User::where('id', $_SESSION['user']['id'])->first();
         $articlesChart = (new ArticleController())->articlesChart();
-        return view('profile.updateUser',compact('user','articlesChart'));
+        return view('profile.updateUser', compact('user', 'articlesChart','usersChart'));
     }
 
-    public function updateUser(Request $request){
+    public function updateUser(Request $request)
+    {
 
-        if(isset($request['name'])){
-            $user = User::where('id',$_SESSION['user']['id'])->first();
+        if (isset($request['name'])) {
+            $user = User::where('id', $_SESSION['user']['id'])->first();
             $user->name = $request['name'];
             $user->save();
 
             return redirect(route('userSettings'));
         }
 
-        if(isset($request['description'])){
+        if (isset($request['description'])) {
 
             $description = $request->validate([
                 'description' => 'required|string'
             ]);
 
-            $user = User::where('id',$_SESSION['user']['id'])->first();
+            $user = User::where('id', $_SESSION['user']['id'])->first();
             $user->description = $description['description'];
             $user->save();
 
             return redirect(route('userSettings'));
         }
 
-        if(isset($request['email'])) {
+        if (isset($request['email'])) {
 
-//            $mail = [
-//                'email' => 'required|email|unique:users'
-//            ];
-//            Validator::make($request->all(), $mail);
-
-
-            $user = User::where('id',$_SESSION['user']['id'])->first();
+            $user = User::where('id', $_SESSION['user']['id'])->first();
             $user->email = $request['email'];
             $user->save();
 
             return redirect(route('userSettings'));
         }
 
-        if(isset($request['password']) && isset($request['password_confirm'])){
+        if (isset($request['password']) && isset($request['password_confirm'])) {
 
             $request->validate([
                 'password' => 'required|min:9',
                 'password_confirm' => 'required|same:password',
             ]);
 
-
-
-//            $user = User::where('id',$_SESSION['user']['id'])->first();
-//            $user->password = Hash::make($pwd['password']);
-//            $user->save();
-
-            $user = User::where('id',$_SESSION['user']['id'])->first();
-            $user->password = Hash::make($request['password']);
-            $user->save();
-
-            return redirect(route('userSettings'));
-        }
-        if($statistic_name === 'comments'){
-            $user = User::find($id);
-            $articlesChart = (new ArticleController)->articlesChart();
-
-            $comments = Comments::where('user_id',$id)->get();
-            foreach($comments as $comment){
-                (new ArticleController)->dateOutput($comment);
-            }
-
-            return view('profile.userInfo',compact('comments','user','articlesChart'));
-        }
-    }
-
-    public function userSettingView(){
-        $user = User::where('id',$_SESSION['user']['id'])->first();
-        $articlesChart = (new ArticleController())->articlesChart();
-        $usersChart = (new ArticleController())->usersChart();
-        return view('profile.updateUser',compact('user','articlesChart','usersChart'));
-    }
-
-    public function updateUser(Request $request){
-
-        if(isset($request['name'])){
-            $user = User::where('id',$_SESSION['user']['id'])->first();
-            $user->name = $request['name'];
-            $user->save();
-
-            return redirect(route('userSettings'));
-        }
-
-        if(isset($request['description'])){
-
-            $description = $request->validate([
-                'description' => 'required|string'
-            ]);
-
-            $user = User::where('id',$_SESSION['user']['id'])->first();
-            $user->description = $description['description'];
-            $user->save();
-
-            return redirect(route('userSettings'));
-        }
-
-        if(isset($request['email'])) {
-
-//            $mail = [
-//                'email' => 'required|email|unique:users'
-//            ];
-//            Validator::make($request->all(), $mail);
-
-
-            $user = User::where('id',$_SESSION['user']['id'])->first();
-            $user->email = $request['email'];
-            $user->save();
-
-            return redirect(route('userSettings'));
-        }
-
-        if(isset($request['password']) && isset($request['password_confirm'])){
-
-            $request->validate([
-                'password' => 'required|min:9',
-                'password_confirm' => 'required|same:password',
-            ]);
-
-
-
-//            $user = User::where('id',$_SESSION['user']['id'])->first();
-//            $user->password = Hash::make($pwd['password']);
-//            $user->save();
-
-            $user = User::where('id',$_SESSION['user']['id'])->first();
+            $user = User::where('id', $_SESSION['user']['id'])->first();
             $user->password = Hash::make($request['password']);
             $user->save();
 
             return redirect(route('userSettings'));
         }
     }
+
+
 }
