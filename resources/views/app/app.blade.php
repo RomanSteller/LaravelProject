776e82f287b4f8663e5c679be2b3e553a4bb669e
Roman <romansteller.csgo@gmail.com>
2021-06-12 04:45:09 +0300

    user svg

--- resources/views/app/app.blade.php
+++ resources/views/app/app.blade.php
@@ -26,8 +26,14 @@
         @else
             <div class="header_user_nickname">
                 <ul>
-                    <li class="top-li" id="top-li"><a href="#" class="user-a">{{$_SESSION['user']['login']}}</a>
+                    <li class="top-li" id="top-li"><a href="#" class="user-a">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                            </svg></a>
+                    </li>
                         <ul class="child-ul" id="child-ul">
+                            <li></li>
                             <li class="li"><a href="{{route('getUserArticles',['id'=>$_SESSION['user']['id'],'statistic_name'=>'articles'])}}" class="li-a">Профиль</a></li>
                             <li class="li"><a href="" class="li-a">Мои публикации</a></li>
 
